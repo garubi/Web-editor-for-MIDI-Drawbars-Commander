3a2af546a6e30c0a3bf8e7217553e2af96968d8d
Stefano <sgaruti@gmail.com>
2020-03-09 15:19:33 +0100

    implement X_SET_PARAM, X_SET_CTRL_PARAMS, X_ERROR_(...) codes

--- js/app.js
+++ js/app.js
@@ -6,7 +6,14 @@ var DWC_MIDI_PRODUCT_ID			=	0x09;
 
 var DWC_MANUF = [DWC_MIDI_MANUF_ID_1, DWC_MIDI_MANUF_ID_2, DWC_MIDI_PRODUCT_ID];
 
-// F0 X_MANID1 X_MANID2 X_PRODID ACTION OBJECT vv F7
+/* ************************************************************************
+ *  SysEX implementation
+ *
+ *  Format for the requests :
+ *  F0 X_MANID1 X_MANID2 X_PRODID X_REQ OBJECT vv F7
+ * Format for the reply
+ * F0 X_MANID1 X_MANID2 X_PRODID X_REP OBJECT (Reply COdes) vv F7
+ */
 
 var X_REQ = 0x00; // Request
 var X_REP = 0x01; // Replay
@@ -16,12 +23,21 @@ var X_ACTIVE_PRESET 	= 0x02; // The active preset. Replay vv is byte) Active pre
 
 var X_REQ_CTRL_PARAMS 	= 0x10; // Current settings for a control: PRESET_ID CTRL_ID. Reply vv is: PRESET_ID CTRL_ID UPP_Type UPP_Prm UPP_Min UPP_Max UPP_Ch UPP_Behaviour LOW_Type LOW_Prm LOW_Min LOW_Max LOW_Ch LOW_Behaviour ALT_Type ALT_Prm ALT_Min ALT_Max ALT_Ch ALT_Behaviour
 var X_SET_CTRL_PARAMS 	= 0x11; // Send the settings for a control (but doesn't save it): PRESET_ID CTRL_ID UPP_Type UPP_Prm UPP_Min UPP_Max UPP_Ch UPP_Behaviour LOW_Type LOW_Prm LOW_Min LOW_Max LOW_Ch LOW_Behaviour ALT_Type ALT_Prm ALT_Min ALT_Max ALT_Ch ALT_Behaviour. Reply vv is 0 if is all right, an Error code if something went wrog
-
+var X_SET_PARAM 		= 0x12; // Send a setting for a single parameter (but doesn't save it): PRESET_ID CTRL_ID PARAM_ID (0-18) param value;
 var X_CMD_SAVE_PRESET	= 0x7F; // Save the Preset to the non volative memory: vv is PRESET_ID. Reply vv is 0 if all went ok, an error code if someting wen wrong
 
 var X_OK = 0x00;
-var X_ERROR = 0x01; // Something went wrong
+var X_ERROR = 0x7F; // Something went wrong
 
+/*
+ * ERROR CODES
+ */
+  var X_ERROR_UNKNOWN = 0x7F;
+  var X_ERROR_PRESET  = 0x10;
+  var X_ERROR_CONTROL = 0x20;
+  var X_ERROR_PARAM   = 0x30;
+
+var error = null;
 var midi_connected = false // store the connection status to avoid two "I'm connected"
 var midi_disconnected = false // store the connection status to avoid two "I'm disconnected"
 var NUM_PRESETS = 4;
@@ -101,7 +117,19 @@ function req_controls( preset_id, ctrl_id ){
 	console.log('Reqesting control: ', ctrl_id);
 	to_dwc.sendSysex( DWC_MANUF, [X_REQ, X_REQ_CTRL_PARAMS, preset_id, ctrl_id ] )
 }
-
+function set_controls( preset_id, ctrl_id ){
+	console.log('Set control: ', ctrl_id);
+	//to_dwc.sendSysex( DWC_MANUF, [X_REQ, X_SET_CTRL_PARAMS, preset_id, ctrl_id ] )
+}
+function set_param( preset_id, ctrl_id, param_id, value ){
+	console.log('Set parameter: ', param_id);
+	console.log('value:', value);
+	console.log('ctrl_id: ',ctrl_id);
+	to_dwc.sendSysex( DWC_MANUF, [X_REQ, X_SET_PARAM, preset_id, ctrl_id, param_id, value ] )
+}
+function raise_error(error){
+	console.log('error', error);
+}
 function parse_sysex( e ){
 	console.log();('Received SysEx:', e.data );
 
@@ -109,28 +137,39 @@ function parse_sysex( e ){
 	if ( e.data[4] != X_REP ) return null; // Discard all messages that are not a reply
 
 	var message_type = e.data[5];
-	// console.log( 'message_type', message_type );
-	// console.log( 'unknown sysex data:', e.data);
+	var reply = e.data[6];
+	if( reply == X_ERROR ){
+		error = e.data[7];
+	}
+	else{
+		error = null;
+	}
+	console.log( 'message_type', message_type );
+	console.log( 'sysex data:', e.data);
 
 	var data = Object.values(e.data);
-	data = data.slice( 6, -1 );
+	data = data.slice( 7, -1 );
 
 	switch ( message_type ) {
 		case X_FW_VER:
-			if( data.length != 3 ) return X_ERROR;
+		console.log('receive version');
+			if( error )return raise_error(error);
+			if( data.length != 3 )return raise_error(error);
 			var version = data.join('.');
 			console.log('version:', version);
 			$('#fw_version_label').text( version );
 			req_active_preset();
 		break;
 		case X_ACTIVE_PRESET:
-			if( data.length != 1 || data[0] > NUM_PRESETS-1 ) return X_ERROR;
+			if( error )return raise_error(error);
+			if( data.length != 1 || data[0] > NUM_PRESETS-1 )return raise_error('invalid preset id');
 			ACTIVE_PRESET = data[0];
 			console.log('active preset: ', ACTIVE_PRESET);
 			$('#active_preset').text( ACTIVE_PRESET );
 			req_controls( ACTIVE_PRESET, 0 );
 		break;
 		case X_REQ_CTRL_PARAMS:
+			if( error )return raise_error(error);
 			var preset_id = data[0];
 			if( preset_id != ACTIVE_PRESET ) return null; //Discard the message because it's for a preset we are not editing
 			var ctrl_id = data[1];
@@ -154,11 +193,45 @@ function parse_sysex( e ){
 				else{
 					$('#' + ctrl_id + '_' + param).val(ctrls[param]);
 				}
-
 			}
 			ctrl_id = ctrl_id + 1;
 			if( ctrl_id <= NUM_CTRL ){ //request for all the controls in the preset
 				req_controls( ACTIVE_PRESET, ctrl_id );
+			}
+		break;
+		case X_SET_CTRL_PARAMS:
+			if( error )return raise_error(error);
+			var preset_id = data[0];
+			if( preset_id != ACTIVE_PRESET ) return null; //Discard the message because it's for a preset we are not editing
+			var ctrl_id = data[1];
+			ctrl_id = ctrl_id + 1;
+			if( ctrl_id <= NUM_CTRL ){ //request for all the controls in the preset
+				set_controls( ACTIVE_PRESET, ctrl_id );
+			}
+		break;
+		case X_SET_PARAM:
+			console.log('reply to X_SET_PARAM');
+			if( error )return raise_error(error);
+			if( data.length != 4 )return raise_error('invalid parameter data');
+			var preset_id = data[0];
+			if( preset_id != ACTIVE_PRESET ) return null; //Discard the message because it's for a preset we are not editing
+			var ctrl_id = data[1];
+			var param_id = data[2];
+			var param_value = data[3];
+			console.log('value:', param_value);
+			if( param_id == 5 || param_id == 11 || param_id == 17 ){ // this are the checkboxes
+				if( param_value & IS_GLOBAL){
+					$('#' + ctrl_id + '_' + param_id + '_global').attr('checked', 'checked');
+				}
+				if( param_value & SEND_BOTH){
+					$('#' + ctrl_id + '_' + param_id + '_both').attr('checked', 'checked');
+				}
+				if( param_value & IS_TOGGLE){
+					$('#' + ctrl_id + '_' + param_id + '_toggle').attr('checked', 'checked');
+				}
+			}
+			else{
+				$('#' + ctrl_id + '_' + param_id).val(param_value);
 			}
 		break;
 		default:
@@ -166,3 +239,13 @@ function parse_sysex( e ){
 		break;
 	}
 }
+
+$('.preset_form input,select').change(function(c) {
+	// console.log('changed');
+	var id = this.id;
+	var ind = id.split("_");
+	var ctrl_id = ind[0];
+	var param_id = ind[1];
+	var value = $( '#' + this.id).val();
+	set_param( ACTIVE_PRESET, ctrl_id, param_id, value);
+});
